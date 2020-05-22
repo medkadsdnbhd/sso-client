0de5b125a8be86a2c556c2aa24ea4e0869d8aca6
MUHAMMAD TARMIZI SANUSI AFKAR <realtarmizisanusi@gmail.com>
2020-05-23 00:31:26 +0800

    add Service Provider and update composer

---
+++ src/SsoServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Medkad\Sso;
+
+use Illuminate\Support\ServiceProvider;
+
+class SsoServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
